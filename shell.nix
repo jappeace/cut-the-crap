312a321b07ef1d15b801b535d209479fc96e3215
Jappie Klooster <jappieklooster@hotmail.com>
2019-02-19 21:09:38 +0100

    Now detect native dependencies
    yes this is the whole point of using nix..

--- shell.nix
+++ shell.nix
@@ -14,15 +14,21 @@
 }:
 let 
     build = pkgs.haskellPackages.callPackage ./default.nix { };
+    buildInputs = [
+        pkgs.haskellPackages.hpack
+        pkgs.haskellPackages.hspec-discover
+        pkgs.cabal-install
+        pkgs.ghc
+        pkgs.cabal2nix
+    ] ++ build.buildInputs;
 in 
-pkgs.stdenv.mkDerivation {
-  name = "db-field";
-  buildInputs = [
-    pkgs.haskellPackages.hpack
-    pkgs.haskellPackages.hspec-discover
-    pkgs.cabal-install
-    pkgs.ghc
-    pkgs.cabal2nix
-  ] ++ build.buildInputs;
+pkgs.mkShell {
+  name = "template";
+  inherit buildInputs;
+  # allow cabal to discover native inputs
+  shellHook = ''
+       export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH
+       export LANG=en_US.UTF-8
+  '';
     
 }
