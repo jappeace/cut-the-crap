c818a1dd1567f8095acb64570009bf4be61e03bd
Jappie Klooster <jappieklooster@hotmail.com>
2019-02-19 21:08:19 +0100

    Shell regeneartes default.nix before doing any cabal stuff

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -9,5 +9,6 @@ set -x
 # (loading currenlty takes quite a bit of time)
 while nix-shell --pure -p pkgs.inotify-tools --run  "inotifywait -r -e modify -e create -e attrib ./src ./app";
 do
+    nix-shell --pure -p pkgs.cabal2nix pkgs.haskellPackages.hpack --run "make update-cabal" &&
     make run-in-shell RUN="make build"
 done
