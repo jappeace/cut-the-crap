c3c17d38be5db20f4e3bed4bacdbabe12a940d3a
Jappie Klooster <jappieklooster@hotmail.com>
2019-07-15 16:58:25 +0200

    Also watch pin

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -7,7 +7,7 @@ set -x
 # TODO make this with tmux http://man7.org/linux/man-pages/man1/tmux.1.html
 # we can leave the nix shells open in that for faster builds
 # (loading currenlty takes quite a bit of time)
-while nix-shell --pure -p pkgs.inotify-tools --run  "inotifywait -r -e modify -e create -e attrib ./src ./app ./package.yaml ./shell.nix";
+while nix-shell --pure -p pkgs.inotify-tools --run  "inotifywait -r -e modify -e create -e attrib ./src ./app ./package.yaml ./shell.nix ./pin.nix";
 do
     nix-shell --pure -p pkgs.cabal2nix pkgs.haskellPackages.hpack --run "make update-cabal" &&
     make run-in-shell RUN="make build"
