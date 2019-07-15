a0e1a96e2c8ec65317757c2142202f4302b38dea
Jappie Klooster <jappieklooster@hotmail.com>
2019-07-15 17:01:42 +0200

    HAX for dir selection

--- scripts/send.sh
+++ scripts/send.sh
@@ -10,10 +10,12 @@ set -x
 
 # TODO use: https://github.com/schell/steeloverseer#readme
 # I think it does this better
+
+TMPDIR="/tmp/"`pwd | sed "s/.*\///"`
 while nix-shell --pure -p pkgs.inotify-tools --run  "inotifywait -r -e modify -e create ./backend ./common ./frontend ./test ./packages ./experiment ./opensource ./default.nix  ./pin.nix";
 do
-    ssh jappie@192.168.0.155 "mkdir -p /tmp/raster"
-    rsync --delete --exclude 'dist-newstyle' --exclude '.git' --exclude 'dist-ghcjs' --rsync-path=/nix/store/5my64lkzh8da3j51cjvpyqn9680vxq0i-system-path/bin/rsync -avz . jappie@192.168.0.155:/tmp/raster
+    ssh jappie@192.168.0.155 "mkdir -p "$TMPDIR
+    rsync --delete --exclude 'dist-newstyle' --exclude '.git' --exclude 'dist-ghcjs' --rsync-path=/nix/store/5my64lkzh8da3j51cjvpyqn9680vxq0i-system-path/bin/rsync -avz . jappie@192.168.0.155:$TMPDIR
     sleep 1
-    rsync --delete --exclude 'dist-newstyle' --exclude '.git' --exclude 'dist-ghcjs' --rsync-path=/nix/store/5my64lkzh8da3j51cjvpyqn9680vxq0i-system-path/bin/rsync -avz . jappie@192.168.0.155:/tmp/raster
+    rsync --delete --exclude 'dist-newstyle' --exclude '.git' --exclude 'dist-ghcjs' --rsync-path=/nix/store/5my64lkzh8da3j51cjvpyqn9680vxq0i-system-path/bin/rsync -avz . jappie@192.168.0.155:$TMPDIR
 done
