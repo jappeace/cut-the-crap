3060ef8c2729bdc574f9fca1e68d537a287705d9
Jappie Klooster <jappieklooster@hotmail.com>
2019-10-20 12:39:33 +0200

    Better pin syntax, upgrade pin

--- pin.nix
+++ pin.nix
@@ -1,14 +1,13 @@
 let 
-hostPkgs = import <nixpkgs> {};
-pinnedPkgs = hostPkgs.fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs-channels";
-    # nixos-unstable as of 15.06.2019
-    rev = "1601f559e89ba71091faa26888711d4dd24c2d4d";
-    sha256 = "0iayyz9617mz6424spwbi9qvmcl8hiql42czxg8mi4ycq4p1k0dx";
-};
+pinnedPkgs = 
+    (builtins.fetchGit {
+    # Descriptive name to make the store path easier to identify
+    name = "nixos-pin-19.10.2019";
+    url = https://github.com/nixos/nixpkgs/;
+    rev = "f203d50d09052792866f926e7c2d3b3bf76d3388";
+    }) ;
 in
 import pinnedPkgs {
     # since I also use this for clients I don't want to have to care
-    config.allowUnfree = true; # took me to long to figure out
+    config.allowUnfree = true; # took me too long to figure out
 }
