078c201e167085db55d05e5d677d2a7906f87f43
https://jappieklooster.nl <jappieklooster@hotmail.com>
2019-10-25 13:52:07 +0200

    Add cabal install too shell

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 let 
     origBuild = pkgs.haskellPackages.callPackage ./default.nix { };
     wTools = pkgs.haskell.lib.overrideCabal origBuild (drv: {
-        libraryToolDepends = drv.libraryToolDepends ++ [ pkgs.ghcid ];
+        libraryToolDepends = drv.libraryToolDepends ++ [ pkgs.ghcid pkgs.cabal-install ];
     });
 in 
 wTools.env
