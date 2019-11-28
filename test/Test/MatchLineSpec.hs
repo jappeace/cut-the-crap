c7f254a51782c4f282586d49f4847fbd95ce7429
Jappie Klooster <jappieklooster@hotmail.com>
2019-11-29 00:40:25 +0100

    Hlint issue

--- test/Test/MatchLineSpec.hs
+++ test/Test/MatchLineSpec.hs
@@ -14,7 +14,7 @@ spec = describe "Match a line" $ do
   it "Should match the end line as well"
     $          getEnd
                  "[silencedetect @ 0x23a94e0] silence_end: 148.515 | silence_duration: 0.825079"
-    `shouldBe` (148.515)
+    `shouldBe` 148.515
   it "Should match the duration"
     $          getDuration
                  "[silencedetect @ 0x23a94e0] silence_end: 148.515 | silence_duration: 0.825079"
