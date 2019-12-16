0ea8a0a0fd3e8d85f44337f13ef2445d22d1bf97
Jappie Klooster <jappieklooster@hotmail.com>
2019-12-16 20:12:32 +0100

    Make ffmpeg available for cut-the-crap

--- overlay/cut-the-crap.nix
+++ overlay/cut-the-crap.nix
@@ -1,25 +1,25 @@
 { mkDerivation, base, exceptions, fetchgit, generic-lens, hpack
 , hspec, hspec-core, lens, optparse-applicative, optparse-generic
 , regex-tdfa, shelly, stdenv, system-filepath, temporary, text
-, unliftio-core, ffmpeg
+, unliftio-core, ffmpeg, makeWrapper
 }:
-mkDerivation {
+(mkDerivation {
   pname = "cut-the-crap";
   version = "1.0.0";
   src = fetchgit {
     url = "https://github.com/jappeace/cut-the-crap";
-    sha256 = "0hmbpd6rsn09gawam6sahdp79nv8480qg6wir5xhd4g494sq78an";
-    rev = "7882e7ec94d616f9b25cc121cfba088ed220f032";
+    sha256 = "1sqfd1fjr8y29i47ak6qnq2axbjqvbqgm8mjgapylfnzwrlwyd5h";
+    rev = "53d64d359f5973ce08bfcdf5f421a2d752b995ec";
     fetchSubmodules = true;
   };
   isLibrary = true;
   isExecutable = true;
+  buildDepends = [ makeWrapper ];
   libraryHaskellDepends = [
     base exceptions generic-lens lens optparse-applicative
     optparse-generic regex-tdfa shelly system-filepath temporary text
     unliftio-core
   ];
-  buildDepends = [ffmpeg];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
     base exceptions generic-lens lens optparse-applicative
@@ -33,4 +33,10 @@ mkDerivation {
   ];
   prePatch = "hpack";
   license = stdenv.lib.licenses.mit;
-}
+  postInstall = ''
+     wrapProgram "$out/bin/cut-the-crap" \
+      --prefix PATH ":" "${ffmpeg}/bin"
+  '';
+
+})
+
