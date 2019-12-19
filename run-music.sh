02607abe227ddab24bcd4cf62a43db1c380753af
Jappie Klooster <jappieklooster@hotmail.com>
2019-12-19 22:53:00 +0100

    Update script as well

--- run-music.sh
+++ run-music.sh
@@ -7,7 +7,7 @@ echo "script that will process a folder with streams";
 files=$(ls $1);
 for i in $files; do 
     echo "Processing "$i;
-    cabal new-run exe -- \
+    cabal new-run cut-the-crap -- \
             --inFile $1/$i \
             --outFile no-crap-$i \
             --voiceTrack 2 \
