c233e9e9155a191a238ea956dbde02c25243c1e2
Jappie Klooster <jappieklooster@hotmail.com>
2019-12-30 22:25:26 +0100

    Upgrade

--- overlay/cut-the-crap.nix
+++ overlay/cut-the-crap.nix
@@ -1,20 +1,19 @@
 { mkDerivation, base, exceptions, fetchgit, generic-lens, hpack
 , hspec, hspec-core, lens, optparse-applicative, optparse-generic
 , regex-tdfa, shelly, stdenv, system-filepath, temporary, text
-, unliftio-core, ffmpeg, makeWrapper
+, unliftio-core
 }:
-(mkDerivation {
+mkDerivation {
   pname = "cut-the-crap";
   version = "1.0.0";
   src = fetchgit {
     url = "https://github.com/jappeace/cut-the-crap";
-    sha256 = "1z4s0zan421q4722k32p0n33bsh7di1m6pbbdzqi7j87lfd9i7nk";
-    rev = "0ea8a0a0fd3e8d85f44337f13ef2445d22d1bf97";
+    sha256 = "0l294rvlmslvp4c120d4p6aravg22r4pwm0zqhqjj7vj2w4js6h7";
+    rev = "2a87fa606e68f0a80cac56d25fe447e6c292620d";
     fetchSubmodules = true;
   };
   isLibrary = true;
   isExecutable = true;
-  buildDepends = [ makeWrapper ];
   libraryHaskellDepends = [
     base exceptions generic-lens lens optparse-applicative
     optparse-generic regex-tdfa shelly system-filepath temporary text
@@ -32,11 +31,6 @@
     system-filepath temporary text unliftio-core
   ];
   prePatch = "hpack";
+  description = "Cut the crap is an automatic video editing program for streamers. It can cut out uninteresting parts by detecting silences.";
   license = stdenv.lib.licenses.mit;
-  postInstall = ''
-     wrapProgram "$out/bin/cut-the-crap" \
-      --prefix PATH ":" "${ffmpeg}/bin"
-  '';
-
-})
-
+}
