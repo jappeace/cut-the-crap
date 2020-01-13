1a5c56ed9afdbea1e6f36ead0660e2cc58032faa
Jappie Klooster <jappieklooster@hotmail.com>
2020-01-13 20:06:43 +0100

    We require built tools to build

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,6 +1,8 @@
 self: super:
 {
   cut-the-crap = super.haskell.lib.overrideCabal (super.haskellPackages.callPackage ./cut-the-crap.nix {}) (drv: {
+
+  buildDepends = [ super.makeWrapper ];
   postInstall = ''
      wrapProgram "$out/bin/cut-the-crap" \
       --prefix PATH ":" "${super.ffmpeg}/bin"
