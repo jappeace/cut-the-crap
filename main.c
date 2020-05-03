e3e810875f5e620519c58f2bb77d649a369975ed
Jappie Klooster <jappieklooster@hotmail.com>
2020-05-03 22:16:27 +0200

    Figured out how to find timestamps

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <pocketsphinx.h>
+
 #define PPCAT_NX(A, B) A
 
 int main(int argc, char *argv[])
@@ -14,34 +15,45 @@ int main(int argc, char *argv[])
 	return -1;
     }
 
-    const ps_decoder_t* ps = ps_init(config);
-    if (ps == NULL) {
+    const ps_decoder_t* decoder = ps_init(config);
+    if (decoder == NULL) {
 	fprintf(stderr, "Failed to create recognizer, see log for details\n");
 	return -1;
     }
 
     // If you want to create your own version of it: it needs to be a single-channel (monaural), little-endian, unheadered 16-bit signed PCM audio file sampled at 16000 Hz.
-    FILE* fh = fopen("heyo.raw", "rb");
-    if (fh == NULL) {
+    FILE* audioFile = fopen("heyo.raw", "rb");
+    if (audioFile == NULL) {
 	fprintf(stderr, "Unable to open input file goforward.raw\n");
 	return -1;
     }
-    int rv = ps_start_utt(ps);
+    int rv = ps_start_utt(decoder);
     int16 buf[512];
 
-    while (!feof(fh)) {
+    while (!feof(audioFile)) {
 	size_t nsamp;
-	nsamp = fread(buf, 2, 512, fh);
-	rv = ps_process_raw(ps, buf, nsamp, FALSE, FALSE);
+	nsamp = fread(buf, 2, 512, audioFile);
+	rv = ps_process_raw(decoder, buf, nsamp, FALSE, FALSE);
+        printf("cur rv: %i - %i \n", rv, nsamp);
+    }
+    rv = ps_end_utt(decoder);
+
+    
+    for(ps_seg_t* iterator = ps_seg_iter(decoder); iterator != NULL; iterator = ps_seg_next(iterator)){
+        int begin = 0; int end = 0;
+        ps_seg_frames(iterator, &begin, &end);
+        char const* word = ps_seg_word(iterator);
+        printf("%s (%i, %i) ", word, begin, end);
+
     }
 
-    rv = ps_end_utt(ps);
     int32 score;
-    char const * hyp = ps_get_hyp(ps, &score);
+    char const* hyp = ps_get_hyp(decoder, &score);
     printf("Recognized: %s\n", hyp);
+    printf("score: %i\n", score);
 
-    fclose(fh);
-    ps_free(ps);
+    fclose(audioFile);
+    ps_free(decoder);
     cmd_ln_free_r(config);
     printf("outputting results " MODELDIR);
     return 0;
