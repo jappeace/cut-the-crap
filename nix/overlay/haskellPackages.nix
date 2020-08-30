04cd0221cdc1e6e28233fd814b2bde2085fa5657
Jappie Klooster <jappieklooster@hotmail.com>
2020-08-30 16:53:17 +0200

    Format and use self var

--- nix/overlay/haskellPackages.nix
+++ nix/overlay/haskellPackages.nix
@@ -1,16 +1,14 @@
 { pkgs, haskellLib }:
 
 with haskellLib;
-self: super: builtins.intersectAttrs super {
-      cut-the-crap =
-        let path = pkgs.stdenv.lib.makeBinPath [ pkgs.ffmpeg ];
-        in overrideCabal (addBuildTool super.cut-the-crap pkgs.makeWrapper) (_drv: {
-        libraryToolDepends =  [
-            pkgs.haskellPackages.c2hs
-        ];
-        postInstall = ''
-            wrapProgram $out/bin/cut-the-crap \
-            --prefix PATH : "${path}"
-        '';
-        });
+self: super:
+builtins.intersectAttrs super {
+  cut-the-crap = let path = pkgs.stdenv.lib.makeBinPath [ pkgs.ffmpeg ];
+  in overrideCabal (addBuildTool super.cut-the-crap pkgs.makeWrapper) (_drv: {
+    libraryToolDepends = [ self.c2hs ];
+    postInstall = ''
+      wrapProgram $out/bin/cut-the-crap \
+      --prefix PATH : "${path}"
+    '';
+  });
 }
